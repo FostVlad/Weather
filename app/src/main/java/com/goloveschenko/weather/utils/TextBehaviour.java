7ffb29c6144647191e5199173e2737058d3d8799
Vlad <vladislavan23@gmail.com>
2017-09-29 18:18:37 +0300

    work with animation is over

--- app/src/main/java/com/goloveschenko/weather/utils/TextBehaviour.java
+++ app/src/main/java/com/goloveschenko/weather/utils/TextBehaviour.java
@@ -1,7 +1,6 @@
 package com.goloveschenko.weather.utils;
 
 import android.content.Context;
-import android.graphics.Point;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -9,10 +8,8 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
-import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -20,32 +17,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class TextBehaviour extends CoordinatorLayout.Behavior<FrameLayout> {
-    private Context context;
-
     private TextView location;
-    private TextView updateTime;
-    private TextView weatherIcon;
     private TextView details;
-    private TextView humidity;
-    private TextView pressure;
+    private TextView weatherIcon;
+    private TextView updateTime;
     private TextView temp;
-
     private View dividerTop;
     private RecyclerView forecastDay;
     private View dividerBottom;
 
-    private int startYPosition;
-    private int finalYPosition;
-    private int startHeight;
-    private float startToolbarPosition;
-
     private float startIconPos;
-    private float startUpdateTimePos;
-    private float startHumidityPos;
-    private float startPressurePos;
+    private boolean isInitFields;
 
     public TextBehaviour(Context context, AttributeSet attrs) {
-        this.context = context;
+        super(context, attrs);
     }
 
     @Override
@@ -55,59 +40,34 @@ public class TextBehaviour extends CoordinatorLayout.Behavior<FrameLayout> {
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, FrameLayout child, View dependency) {
-        //return if we have non initialized fragment
         if (!initFields(child)) {
             return false;
         }
-        initProperties(child, dependency);
 
         //TOOLBAR HEIGHT
         AppBarLayout appBarLayout = findViewByClass(parent, AppBarLayout.class);
         CollapsingToolbarLayout collapsingToolbarLayout = findViewByClass(appBarLayout, CollapsingToolbarLayout.class);
         Toolbar toolbar = findViewByClass(collapsingToolbarLayout, Toolbar.class);
         CollapsingToolbarLayout.LayoutParams layoutParams = (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
-        layoutParams.height = location.getHeight() + details.getHeight() + temp.getHeight() + dividerTop.getHeight() + forecastDay.getHeight() + dividerBottom.getHeight();
+        layoutParams.height = location.getHeight() + details.getHeight() + dividerTop.getHeight() + forecastDay.getHeight() + dividerBottom.getHeight() + 10; //TODO: fix padding
         toolbar.setLayoutParams(layoutParams);
 
-        final int maxScrollDistance = (int) startToolbarPosition;
-        float expandedPercentageFactor = dependency.getY() / maxScrollDistance;
+        float visibility = weatherIcon.getY() / (int) startIconPos;
 
-        float distanceYToSubtract = ((startYPosition - finalYPosition) * (1f - expandedPercentageFactor)) + child.getHeight();
+        weatherIcon.setY(dependency.getY() - forecastDay.getHeight() - dividerBottom.getHeight() - temp.getHeight() - weatherIcon.getHeight());
+        weatherIcon.setAlpha(visibility);
+        updateTime.setY(dependency.getY() - forecastDay.getHeight() - dividerBottom.getHeight() - updateTime.getHeight());
+        updateTime.setAlpha(visibility);
+        temp.setY(dependency.getY() - forecastDay.getHeight() - dividerBottom.getHeight() - temp.getHeight());
+        temp.setAlpha(visibility);
 
-        //updateTime.setAlpha(expandedPercentageFactor);
-//        updateTime.setAlpha(updateTime.getY() / (int) startUpdateTimePos);
-        updateTime.setAlpha(weatherIcon.getY() / (int) startIconPos);
-
-        //HIDE ICON
-        weatherIcon.setY(dependency.getY() - forecastDay.getHeight() - dividerTop.getHeight() - temp.getHeight() - pressure.getHeight() - humidity.getHeight() - details.getHeight() - weatherIcon.getHeight());
-        weatherIcon.setAlpha(weatherIcon.getY() / (int) startIconPos);
-
-        details.setY(dependency.getY() - forecastDay.getHeight() - dividerTop.getHeight() - temp.getHeight() - pressure.getHeight() - humidity.getHeight() - details.getHeight());
-
-        humidity.setY(dependency.getY() - forecastDay.getHeight() - dividerTop.getHeight() - temp.getHeight() - pressure.getHeight() - humidity.getHeight());
-//        humidity.setAlpha(expandedPercentageFactor);
-//        humidity.setAlpha(humidity.getY()/ (int) startHumidityPos);
-
-        pressure.setY(dependency.getY() - forecastDay.getHeight() - dividerTop.getHeight() - temp.getHeight() - pressure.getHeight());
-//        pressure.setAlpha(expandedPercentageFactor);
-//        pressure.setAlpha(pressure.getY() / (int) startPressurePos);
-
-        temp.setY(dependency.getY() - forecastDay.getHeight() - dividerTop.getHeight() - temp.getHeight());
-
-        dividerTop.setY(dependency.getY() - forecastDay.getHeight() - dividerTop.getHeight());
+        dividerTop.setY(dependency.getY() - forecastDay.getHeight() - dividerBottom.getHeight());
         forecastDay.setY(dependency.getY() - forecastDay.getHeight());
         dividerBottom.setY(dependency.getY());
 
         updateVisibility(child);
 
         return true;
-    }
-
-    private int getScreenHeight() {
-        Display display = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return size.y;
     }
 
     private void updateVisibility(FrameLayout child) {
@@ -123,58 +83,26 @@ public class TextBehaviour extends CoordinatorLayout.Behavior<FrameLayout> {
     }
 
     private boolean initFields(FrameLayout child) {
-        ConstraintLayout layout = findViewByClass(child, ConstraintLayout.class);
-        if (layout != null && layout.getChildCount() != 0) {
-            location = (TextView) layout.getChildAt(0);
-            updateTime = (TextView) layout.getChildAt(1);
-            weatherIcon = (TextView) layout.getChildAt(2);
-            details = (TextView) layout.getChildAt(3);
-            humidity = (TextView) layout.getChildAt(4);
-            pressure = (TextView) layout.getChildAt(5);
-            temp = (TextView) layout.getChildAt(6);
+        if (!isInitFields) {
+            ConstraintLayout layout = findViewByClass(child, ConstraintLayout.class);
+            if (layout != null && layout.getChildCount() != 0) {
+                location = (TextView) layout.getChildAt(0);
+                details = (TextView) layout.getChildAt(1);
+                weatherIcon = (TextView) layout.getChildAt(2);
+                startIconPos = weatherIcon.getY();
+                updateTime = (TextView) layout.getChildAt(3);
+                temp = (TextView) layout.getChildAt(4);
 
-            dividerTop = (View) layout.getChildAt(7);
-            forecastDay = (RecyclerView) layout.getChildAt(8);
-            dividerBottom = (View) layout.getChildAt(9);
+                dividerTop = layout.getChildAt(5);
+                forecastDay = (RecyclerView) layout.getChildAt(6);
+                dividerBottom = layout.getChildAt(7);
 
-            return true;
-        } else {
-            return false;
+                isInitFields = true;
+            } else {
+                isInitFields = false;
+            }
         }
-    }
-
-    private void initProperties(FrameLayout child, View dependency) {
-        if (startYPosition == 0) {
-            startYPosition = (int) (dependency.getY());
-        }
-
-        if (finalYPosition == 0) {
-            finalYPosition = (dependency.getHeight() / 2);
-        }
-
-        if (startHeight == 0) {
-            startHeight = child.getHeight();
-        }
-
-        if (startToolbarPosition == 0) {
-            startToolbarPosition = dependency.getY();
-        }
-
-        if (startIconPos == 0) {
-            startIconPos = weatherIcon.getY();
-        }
-
-        if (startUpdateTimePos == 0) {
-            startUpdateTimePos = updateTime.getY();
-        }
-
-        if (startHumidityPos == 0) {
-            startHumidityPos = humidity.getY();
-        }
-
-        if (startPressurePos == 0) {
-            startPressurePos = pressure.getY();
-        }
+        return isInitFields;
     }
 
     private <T extends View> T findViewByClass(ViewGroup viewGroup, Class currentClass){
